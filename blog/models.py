48bfabd0358364ad96899eb1bd54cb914c9ca059
Anthony Hung Vu <hvu24@horizon.csueastbay.edu>
2023-10-05 11:30:02 -0700

    added admin

--- blog/models.py
+++ blog/models.py
@@ -10,11 +10,12 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
 
-class Post(models.Models):
+class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
     image_name = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name='posts')
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
+    tags = models.ManyToManyField(Tag)
