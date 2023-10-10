676428747d8d5131b750916a1d244ffdc46bec51
Anthony Hung Vu <hvu24@horizon.csueastbay.edu>
2023-10-09 20:13:02 -0700

    created view for comment form submission

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,9 @@ from datetime import date
 from .models import Post
 from django.views.generic import ListView, DetailView
 from .forms import CommentForm
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 class StartingPageView(ListView):
     template_name = "blog/index.html"
@@ -38,15 +41,43 @@ class AllPostViews(ListView):
 #         "all_posts": all_posts
 #     })
 
-class SinglePostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+# class SinglePostView(DetailView):
+#     template_name = "blog/post-detail.html"
+#     model = Post
 
-    def get_context_data(self, **kwargs: Any):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+#     def get_context_data(self, **kwargs: Any):
+#         context = super().get_context_data(**kwargs)
+#         context["post_tags"] = self.object.tags.all()
+#         context["comment_form"] = CommentForm()
+#         return context
+
+class SinglePostView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post":post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        context = {
+            "post":post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form
+        }
+        return render(request, "blog/post-detail.html", context)
 
 # def post_detail(request, slug):
 #     # identified_post = next(post for post in all_posts if post['slug'] ==slug)
