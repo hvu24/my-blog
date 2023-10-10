d4118dbe637081cb8b0fc205d79fd32a67f0540d
Anthony Hung Vu <hvu24@horizon.csueastbay.edu>
2023-10-09 17:02:54 -0700

    converted function views to class views

--- blog/views.py
+++ blog/views.py
@@ -1,94 +1,55 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
 from datetime import date
 from .models import Post
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
-# all_posts = [
-    # {
-    #     "slug": "hike-in-the-mountains",
-    #     "image": "mountains.jpg",
-    #     "author": "Anthony",
-    #     "date": date(2021, 7, 21),
-    #     "title": "Mountain Hiking",
-    #     "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-    #     "content": """
-    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
 
-    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
-    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-    #     """
-    # },
-    # {
-    #     "slug": "programming-is-fun",
-    #     "image": "coding.jpg",
-    #     "author": "Anthony",
-    #     "date": date(2022, 3, 10),
-    #     "title": "Programming Is Great!",
-    #     "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-    #     "content": """
-    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+# def starting_page(request):
+#     latest_posts = Post.objects.all().order_by("-date")[:3] #doesnt support negative indexing
+#     # sorted_posts = sorted(all_posts, key=get_date)
+#     # latest_posts = sorted_posts[-3:]
+#     return render(request, "blog/index.html", {
+#         "posts": latest_posts
+#     })
 
-    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-    #     """
-    # },
-    # {
-    #     "slug": "into-the-woods",
-    #     "image": "woods.jpg",
-    #     "author": "Anthony",
-    #     "date": date(2020, 8, 5),
-    #     "title": "Nature At Its Best",
-    #     "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-    #     "content": """
-    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+class AllPostViews(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
 
-    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+# def posts(request):
+#     all_posts = Post.objects.all().order_by("-date")
+#     return render(request, "blog/all-posts.html", {
+#         "all_posts": all_posts
+#     })
 
-    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-    #     """
-    # }
-# ]
-# def get_date(post):
-#     return post['date']
+class SinglePostView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3] #doesnt support negative indexing
-    # sorted_posts = sorted(all_posts, key=get_date)
-    # latest_posts = sorted_posts[-3:]
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
-    })
-
-def post_detail(request, slug):
-    # identified_post = next(post for post in all_posts if post['slug'] ==slug)
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post,
-        "post_tags": identified_post.tags.all()
-    })
+# def post_detail(request, slug):
+#     # identified_post = next(post for post in all_posts if post['slug'] ==slug)
+#     identified_post = get_object_or_404(Post, slug=slug)
+#     return render(request, "blog/post-detail.html", {
+#         "post": identified_post,
+#         "post_tags": identified_post.tags.all()
+#     })
